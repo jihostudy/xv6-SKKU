ba836e517a5211f92022cd22963170634f944428
jihokim <2019311945@swji.skku.edu>
2023-10-14 11:30:57 +0900

    PA1 (Booting Message)

--- init.c
+++ init.c
@@ -21,6 +21,9 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1, "Student ID: 2019311945\n");
+    printf(1, "Name: Jiho Kim\n");
+    printf(1, "==========Any Message==========\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
